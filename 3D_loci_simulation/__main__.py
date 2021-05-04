d22b7444d970c396dd95de8c87f86e174df0f324
JB <fiche@cbs.cnrs.fr>
2021-05-04 16:41:13 +0200

    Chose between homoegneous and inhomogenous background simulation

--- 3D_loci_simulation/__main__.py
+++ 3D_loci_simulation/__main__.py
@@ -7,12 +7,13 @@ Created on Tue Apr 27 11:37:43 2021
 """
 
 import yaml
-import os, datetime
+import os
 from datetime import datetime
 import os.path as path
 import matplotlib.pyplot as plt
 import glob
 import numpy as np
+from shutil import copyfile
 
 # Open the configuration file
 # ---------------------------
@@ -55,8 +56,17 @@ simulationdir = os.path.join(
 os.mkdir(simulationdir)
 os.chdir(simulationdir)
 
+# Create the folder structure for the deconvolution
+# -------------------------------------------------
+
+os.mkdir('To_deconvolve')
+os.mkdir('To_deconvolve/folder_1')
+os.mkdir('To_deconvolve/folder_1/folder_2')
+
 nROI = config_parameters["acquisition_data"]["nROI"]
 for roi in range(nROI):
+
+    print('simulate ROI #{} :'.format(roi))
 
     # Generate the loci coordinates
     # -----------------------------
@@ -69,14 +79,28 @@ for roi in range(nROI):
 
     N_false_positive = config_parameters["detection"]["number_false_positive_data"]
     n_fp = int(np.random.normal(N_false_positive, 1000, 1))
-    fp_coordinates = _loci.define_false_positive_coordinates(n_fp)
+    if np.random.binomial(1, 0.8) == 0:
+        print('Homogeneous')
+        fp_coordinates = _loci.define_homogeneous_bkg_coordinates(n_fp)
+    else:
+        print('Inhomogeneous')
+        fp_coordinates = _loci.define_inhomogeneous_bkg_coordinates(n_fp)
 
     # Generate the images
     # -------------------
 
     _stack = SimulateData(config_parameters, n_locus, loci_coordinates, n_fp, fp_coordinates, psf_files)
     _stack.create_bkg_stack()
-    _stack.simulate_raw_stack(roi)
+    movie_name = _stack.simulate_raw_stack(roi)
     _stack.create_ellipsoid_template()
     _stack.simulate_ground_truth(roi)
 
+    # Make a copy of the stack in a folder for the deconvolution
+    # ----------------------------------------------------------
+
+    folder_name = 'ROI_' + str(roi)
+    new_dir = os.path.join(simulationdir, 'To_deconvolve/folder_1/folder_2', folder_name)
+    os.mkdir(new_dir)
+
+    new_path = new_dir + '/' + movie_name
+    copyfile(movie_name, new_path)
